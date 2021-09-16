2ef05aa902dc530cd131f75cf08c3f3416af9233
Noah Spencer <noahspencer0522@gmail.com>
2021-09-16 12:32:20 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 let cats = ['Milo', 'Otis', 'Garfield']
 function destructivelyAppendCat(name){
-    cats.push('Ralph')
-    cats.unshift('Bob')
+    cats.push(name)
 }
-
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    
+    
+}
